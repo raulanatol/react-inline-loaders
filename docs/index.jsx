21b64603e575ea233919dfa8fbc59ec1c3fae5df
raulanatol <raul@natol.es>
2017-01-23 09:23:18 +0000

    gh-pages error fix

--- docs/index.jsx
+++ docs/index.jsx
@@ -32,25 +32,16 @@ const documentationImports = {
 const title = `${NAME} v${VERSION}`; // eslint-disable-line no-undef
 const project = `${USER}/${NAME}`; // eslint-disable-line no-undef
 const pages = [
-  {
-    path: '/',
-    title: 'Introduction',
-    imports: documentationImports,
-    component: require('catalog/lib/loader!raw!../README.md')
-  },
-  {
-    title: 'Demo',
-    pages: [
-      { path: '/demos/DotWaveLoader', title: 'DotWaveLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/DotWaveLoader.md') },
-      { path: '/demos/CenterSquaresLoader', title: 'CenterSquaresLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/CenterSquaresLoader.md') },
-      { path: '/demos/SimpleSpinnerLoader', title: 'SimpleSpinnerLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/SimpleSpinnerLoader.md') },
-      { path: '/demos/LittleDotLoader', title: 'LittleDotLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/LittleDotLoader.md') },
-      { path: '/demos/ChasingSquaresLoader', title: 'ChasingSquaresLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/ChasingSquaresLoader.md') },
-      { path: '/demos/DotLineLoader', title: 'DotLineLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/DotLineLoader.md') },
-      { path: '/demos/RotateCirclesLoader', title: 'RotateCirclesLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/RotateCirclesLoader.md') }
-    ]
-  }
+  { path: '/', title: 'Introduction', imports: documentationImports, component: require('catalog/lib/loader!raw!../README.md') },
+  { path: '/demos/DotWaveLoader', title: 'DotWaveLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/DotWaveLoader.md') },
+  { path: '/demos/CenterSquaresLoader', title: 'CenterSquaresLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/CenterSquaresLoader.md') },
+  { path: '/demos/SimpleSpinnerLoader', title: 'SimpleSpinnerLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/SimpleSpinnerLoader.md') },
+  { path: '/demos/LittleDotLoader', title: 'LittleDotLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/LittleDotLoader.md') },
+  { path: '/demos/ChasingSquaresLoader', title: 'ChasingSquaresLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/ChasingSquaresLoader.md') },
+  { path: '/demos/DotLineLoader', title: 'DotLineLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/DotLineLoader.md') },
+  { path: '/demos/RotateCirclesLoader', title: 'RotateCirclesLoader', imports: documentationImports, component: require('catalog/lib/loader!raw!./demos/RotateCirclesLoader.md') }
 ];
+
 
 // Catalog - logoSrc="../images/logo.png"
 ReactDOM.render(
